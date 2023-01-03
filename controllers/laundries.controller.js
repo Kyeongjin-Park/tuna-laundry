b4c5b125f36f293ef9fea2b1c9f517745d3aef67
Kyeongjin-Park <cloud1178@naver.com>
2023-01-03 13:42:28 +0900

    Add laundry controller and service.

--- controllers/laundries.controller.js
+++ controllers/laundries.controller.js
@@ -4,36 +4,78 @@ const LaundryService = require("../services/laundries.services");
 // Laundry의 컨트롤러 역할을 하는 클래스
 class LaundriesController {
   LaundryService = new LaundryService(); // Laundry 서비스 클래스를 컨트롤러 클래스의 멤버 변수로 할당.
-
-  getLaundries = async(req, res, next) => {
+  
+  // 세탁물 전체 조회
+  getLaundries = async(req, res) => {
     try {
-      
+      const laundries = async(req, res) => {
+        const laundries = await this.laundry.service.findAllLaundry();
+
+        res.status(200).send({ "DB 입력" });
+      }
     } catch (error) {
-      res.status(401).send({errorMessage:error})
+      console.log(error);
+      res.status(401).send({errorMessage:error});
     }
   }
 
-  postLaundries = async(req, res, next) => {
+  // 특정 세탁물 조회
+  getLaundryById = async(req, res) => {
+    const { laundryId } = req.params;
+    const laundry = await this.LaundryService.findLaundryById(laundryId);
+
+    res.status(200).send({ "DB 입력" });
+  }
+
+  // 세탁물 등록
+  createLaundry = async(req, res) => {
     try {
-      
+      const { } = req.body;
+      const createLaundryData = await this.LaundryService.createLaundry(
+
+      );
+
+      res.status(200).send({ "DB 입력" });
+
     } catch (error) {
-      res.status(401).send({errorMessage:error})
+      console.log(error);
+      res.status(401).send({errorMessage:error});
     }
   }
 
-  updateLaundries = async(req, res, next) => {
+  // 세탁물 내용 수정
+  updateLaundry = async(req, res) => {
     try {
-      
+      const { laundryId } = req.params;
+      const { } = req.body;
+
+      const updateLaundry = await this.LaundryService.updateLaundry(
+
+      );
+
+      res.status(200).send({ "DB 입력" });
+
     } catch (error) {
-      res.status(401).send({errorMessage:error})
+      console.log(error);
+      res.status(401).send({errorMessage:error});
     }
   }
 
-  deleteLaundries = async(req, res, next) => {
+  // 세탁물 취소
+  deleteLaundry = async(req, res) => {
     try {
+      const { laundryId } = req.params;
+      const { } = req.body;
+
+      const deleteLaundry = await this.LaundryService.deleteLaundry(
+
+      );
+
+      res.status(200).send({ "DB 입력" });
       
     } catch (error) {
-      res.status(401).send({errorMessage:error})
+      console.log(error);
+      res.status(401).send({errorMessage:error});
     }
   }
 }
--- services/laundries.services.js
+++ services/laundries.services.js
@@ -1,5 +1,79 @@
 const LaundryRepository = require("../repositories/laundries.repository");
 
+class LaundryService {
+  laundryRepository = new LaundryRepository();
+
+  // 세탁물 전체 보기
+  findAllLaundry = async() => {
+    const allLaundry = await this.laundryRepository.findAllLaundry();
+
+    allLaundry.sort((a,b) => {
+      return b.createdAt - a.createdAt
+    });
+
+    return allLaundry.map((laundry) => {
+      return {
+        laundryId: laundry.laundryId,
+      };
+    });
+  };
+
+  // 특정 세탁물 보기
+  findLaundryById = async(laundryId) => {
+    const findLaundry = await this.laundryRepository.findLaundryById(laundryId);
+
+    return {
+      laundryId: findLaundry.laundryId,
+    };
+  };
+
+  // 세탁물 등록
+  createLaundry = async( inputDB ) => {
+    const createLaundryData = await this.laundryRepository.createLaundry(
+      "DB 입력",
+    );
+
+    return {
+      laundryId: createLaundryData.null,
+    };
+  };
+
+  // 세탁물 내용 변경
+  updateLaundry = async( inputDB ) => {
+    const findLaundry = await this.laundryRepository.findLaundryById(laundryId);
+    if (!findLaundry) throw new Error("세탁물이 존재하지 않습니다.");
+
+    await this.laundryRepository.updateLaundry(
+      "DB 입력"
+    );
+
+    const updateLaundry = await this.laundryRepository.findLaundryById(laundryId);
+
+    return {
+      laundryId: updateLaundry.laundryId,
+    };
+  };
+
+  // 세탁물 삭제
+  deleteLaundry = async( inputDB ) => {
+    const findLaundry = await this.laundryRepository.findLaundryById(laundryId);
+    if (!findLaundry) throw new Error("세탁물이 존재하지 않습니다.");
+
+    await this.laundryRepository.deleteLaundry(
+      "DB 입력"
+    );
+
+    const deleteLaundry = await this.laundryRepository.deleteLaundry(
+      laundryId,
+      password
+    );
+
+    return {
+      laundryId: findLaundry.laundryId,
+    };
+  };
+
+}
 
 
 module.exports = LaundryService;
