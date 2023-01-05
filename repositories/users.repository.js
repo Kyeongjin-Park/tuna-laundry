3c53e6c9b52a0807569fc6b1ec1850b52a962264
junhyuk <wwns1009.gmail.com>
2023-01-05 11:47:32 +0900

    usercommit

--- repositories/users.repository.js
+++ repositories/users.repository.js
@@ -4,13 +4,13 @@ const { User } = require('../models');
 class UserRepository {
   findAllUser = async () => {
     // ORM인 Sequelize에서 Posts 모델의 findAll 메소드를 사용해 데이터를 요청합니다.
-    const user = await User.findAll({where: {nickname}});
+    const user = await User.findAll();
     return user;
   }
 
   findOneUser = async () => {
     
-    const user = await User.findOne({ where: { nickname, password } });
+    const user = await User.findOne();
     return user;
   }
 
