76bc9b73fa2cad57e6b7e8356d1df9f7d4bf79d0
Kyeongjin-Park <cloud1178@naver.com>
2023-01-05 21:32:11 +0900

    오류 수정

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 // 패키지 불러오기
 const express = require("express");
-const router = express.Router();
 const app = express();
-
+app.use(express.urlencoded({ extended: false }));
 
 // router불러오기
 const userRouter = require("./routes/users.routes");
 const laundryRouter = require("./routes/laundries.routes")
-app.use("/", express.urlencoded({ extended: false }), router);
 // .env 사용
 // config() 설정이나 프로그램의 실행 일부 등을 저장해둔 파일.
 require("dotenv").config();
@@ -23,7 +21,7 @@ app.use(express.static(__dirname + "/assets"));
 //   res.send("./assets/templates/login.html");
 // });
 app.get("/", function(req, res) {
-    res.sendFile( __dirname + "/assets/templates/userslaundry.html");
+    res.sendFile( __dirname + "/assets/templates/login.html");
 });
 app.get("/usersignup", function(req, res) {
     res.sendFile( __dirname + "/assets/templates/usersignup.html");
@@ -35,15 +33,8 @@ app.get("/usersmypage", function(req, res) {
     res.sendFile( __dirname + "/assets/templates/usersmypage.html");
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-app.use("/", [laundryRouter])
-=======
->>>>>>> 744dfa0bf31f032bbabd015929f3ae420479313b
-=======
->>>>>>> 744dfa0bf31f032bbabd015929f3ae420479313b
-app.use("/", [userRouter]);
 
+app.use("/", [userRouter, laundryRouter]);
 
 
 
@@ -51,3 +42,6 @@ app.use("/", [userRouter]);
 app.listen(process.env.PORT, () => {
   console.log(`http://127.0.0.1:${process.env.PORT}`);
 });
+
+
+
